0556c5d85f046cb80ce4b4b470d085984b33d3d2
zwarii <zwarii@Webbeds-MacBook-Pro.local>
2023-03-09 14:38:00 -0500

    5

--- controllers/players.js
+++ controllers/players.js
@@ -1,37 +1,52 @@
-// POST (add) a new player to a team
-exports.addPlayerToTeam = (req, res) => {
-    Team.findById(req.params.teamId, (err, team) => {
-      if (err) return res.status(500).send(err);
-      if (!team) return res.status(404).send('Team not found');
-      const player = new Player(req.body);
-      player.save((err) => {
-        if (err) return res.status(500).send(err);
-        team.players.push(player);
-        team.save((err) => {
-          if (err) return res.status(500).send(err);
-          res.send(team);
-        });
-      });
-    });
-  };
-  
-  // PUT (update) an existing player in a team
-  exports.updatePlayerInTeam = (req, res) => {
-    Team.findById(req.params.teamId, (err, team) => {
-      if (err) return res.status(500).send(err);
-      if (!team) return res.status(404).send('Team not found');
-      Player.findByIdAndUpdate(req.params.playerId, req.body, { new: true }, (err, player) => {
-        if (err) return res.status(500).send(err);
-        if (!player) return res.status(404).send('Player not found');
-        res.send(team);
-      });
-    });
-  };
-  
-  // DELETE an existing player from a team
-  exports.removePlayerFromTeam = (req, res) => {
-    Team.findById(req.params.teamId, (err, team) => {
-      if (err) return res.status(500).send(err);
-      if (!team) return res.status(404).send('Team not found');
-      Player.findByIdAnd}
-   ) }
+const Player = require('../models/player');
+
+function getPlayer(req, res) {
+  Player.findById(req.params.id, function(err, player) {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(player);
+    }
+  });
+}
+
+function createPlayer(req, res) {
+  const newPlayer = new Player(req.body);
+  newPlayer.save(function(err, player) {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(player);
+    }
+  });
+}
+
+function updatePlayer(req, res) {
+  Player.findByIdAndUpdate(req.params.id, req.body, { new: true }, function(
+    err,
+    player
+  ) {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(player);
+    }
+  });
+}
+
+function deletePlayer(req, res) {
+  Player.findByIdAndRemove(req.params.id, function(err) {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json({ message: 'Player successfully deleted' });
+    }
+  });
+}
+
+module.exports = {
+  getPlayer,
+  createPlayer,
+  updatePlayer,
+  deletePlayer
+};
